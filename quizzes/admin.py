af1e1339c935f62d735088de997cb3bbe7a767e3
Kevin Stradtman <stradtkt22@gmail.com>
2019-02-07 11:04:48 -0500

    Added to admin

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class QuizCategoryAdmin(admin.ModelAdmin):
+    list_display = ['category']
+
+
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ['name', 'get_category']
+    raw_id_fields = ('category',)
+
+    def get_category(self, obj):
+        return obj.category.category
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question', 'get_quiz']
+    raw_id_fields = ('quiz',)
+
+    def get_quiz(self, obj):
+        return obj.quiz.name
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('choice', 'is_right', 'get_question')
+    raw_id_fields = ('question',)
+
+    def get_question(self, obj):
+        return obj.question.question
+
+
+
+admin.site.register(QuizCategory, QuizCategoryAdmin)
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
--- quizzes/models.py
+++ quizzes/models.py
@@ -8,15 +8,15 @@ class QuizCategory(models.Model):
 
 class Quiz(models.Model):
     name = models.CharField(max_length=50)
-    category = models.ForeignKey(QuizCategory)
+    category = models.ForeignKey(QuizCategory, on_delete=models.CASCADE)
 
 
 class Question(models.Model):
-    quiz = models.ForeignKey(Quiz)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     question = models.CharField(max_length=255)
 
     
 class Answer(models.Model):
-    question = models.ForeignKey(Question)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     is_right = models.BooleanField()
     choice = models.CharField(max_length=255)
